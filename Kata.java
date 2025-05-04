23523ddfb71ba060b4b3409e94a7375760c2e89a
spiffamani <cjdaveeds@gmail.com>
2025-05-04 18:59:42 +0100

    chibuzo_task git commit -m chibuzo_task git commit -m chibuzo_task

---
+++ Kata.java
@@ -0,0 +1,98 @@
+public class Kata {
+public static Boolean isEven(int userinput){
+
+if(userinput % 2 == 0){
+return true;
+}
+else {
+return false;
+}
+}
+}
+
+public static Boolean isprime(int userinput){ 
+
+for(int i = 1 ; i <= userinput; i++)
+
+if (userinput % 2 == 0)
+
+return true;
+
+else 
+return false;
+
+
+}
+}
+
+
+public static Factor(int usernumber){
+int counter;
+for(int count = 1; count <= usernumber; count++){
+if(userinput % 2 == 0)
+++counter;
+}
+return counter;
+}
+
+
+
+
+
+public static  factorial(int userinput)
+int  result = 0;
+
+for(int i = 1; i <= userinput;)
+result += userinput * i;
+return result; 
+
+
+
+public static Square(int number){
+int result;
+result = Math.sqr(userinput)
+return result; 
+
+
+
+
+public static Boolean ispalindrome(int userinput){
+int number1 = 0;
+int number2 = 0;
+int number3 = 0;
+int number4 = 0;
+int number5 = 0;
+
+number1 =  userinput / 10000;
+userinput - number1 * 10000; 
+number2 = userinput / 1000 ;
+userinput - number2 * 1000;
+number3 = userinput / 100;
+userinput - number3 / 100;
+number4 = userinput / 10;
+userinput - number4 / 10;
+number5 = userinput;
+
+ if(number1 == number5){
+return true;
+}
+else{
+return false;
+if(number2 == number4){
+return true;
+}
+else{
+return false;
+}
+
+if(number3 == number3){
+return true;
+}
+else{
+return false;
+}
+
+
+}
+}
+
